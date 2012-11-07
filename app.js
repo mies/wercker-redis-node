052c524d81101c281e7a716e10df09f1072a71f4
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-11-07 11:23:41 +0100

    working example

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var app = express();
 var redis = require('redis-url').connect();
 
+redis.sadd('decepticons', 'megatron');
+redis.sadd('decepticons', 'shockwave');
+redis.sadd('decepticons', 'atrotrain');
+
 app.get('/', function(request, response) {
-  response.send('hi')
+  redis.smembers('decepticons', function(err, value) {
+    response.send(value);
+  });
 });
 
 var port = process.env.PORT || 5000;
