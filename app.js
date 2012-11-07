66b729e33e33c96a9fb318b0bc3fb411c801508a
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-11-07 11:54:32 +0100

    added test

--- app.js
+++ app.js
@@ -15,4 +15,6 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on port " + port);
-})
+});
+
+module.exports = app;
