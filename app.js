75e5ac5b56e6166e406a1560dd88c6ce9706cc79
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-11-12 15:57:36 +0100

    node env

--- app.js
+++ app.js
@@ -30,8 +30,9 @@ app.get('/decepticons.json', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Connecting to Redis...")
-  console.log(process.env.WERCKER_REDIS_HOST + ':' + process.env.WERCKER_REDIS_PORT)
+  console.log("Connecting to Redis...");
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.WERCKER_REDIS_HOST + ':' + process.env.WERCKER_REDIS_PORT);
   console.log("Listening on port " + port);
 });
 
