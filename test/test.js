693fbc3dca2c11a71ff3af04840c3b8f930b3e27
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-11-15 11:40:59 +0100

    should and basic test

--- test/test.js
+++ test/test.js
@@ -1,27 +1,22 @@
-var request = require('supertest')
-  , express = require('express')
-  , redisurl = require('redis-url');
+var redisurl = require('redis-url')
+  , should = require('should');
 
-var app = require('../app.js')
-var connectionString = process.env.WERCKER_REDIS_HOST
-app.redis = redisurl.connect(connectionString);
 
-describe('Home', function(){
-  console.log('print nodeenv')
-  console.log(process.env.NODE_ENV)
-      it('respond with hello cybertron', function(done){
-      request(app)
-      .get('/')
-      .expect(200, done);
-  });
-});
+describe("Decepticons", function() {
+  it("", function(done) {
+    var host = process.env.WERCKER_REDIS_HOST;
 
-describe('Decepticons', function(){
-      it('respond with json', function(done){
-      request(app)
-      .get('/decepticons.json')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
+    var redis = redisurl.connect(host);
+    redis.sadd('decepticons', 'megatron');
+    redis.sadd('decepticons', 'shockwave');
+    redis.sadd('decepticons', 'astrotrain');
+
+    redis.smembers('decepticons', function(err, value) {
+    if (err) {
+      console.log(err);
+    }
+    value.should.eql(['astrotrain', 'shockwave','megatron']);
+    done();
+    });
   });
 });
