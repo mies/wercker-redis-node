2ce980f74349948ac9b3da0155468fd956bd164b
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-11-15 11:43:51 +0100

    should and basic test

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ describe("Decepticons", function() {
     var connectionString = process.env.WERCKER_REDIS_HOST + ':' + process.env.WERCKER_REDIS_PORT;
     var host = process.env.WERCKER_REDIS_HOST;
 
-    var redis = redisurl.connect(host);
+    var redis = redisurl.connect(connectionString);
     redis.sadd('decepticons', 'megatron');
     redis.sadd('decepticons', 'shockwave');
     redis.sadd('decepticons', 'astrotrain');
